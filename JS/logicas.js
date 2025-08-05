83c17c6a9a115d819161c0777a56ef32a6f832f2
FDarko74 <frederico.marcel@gmail.com>
2025-08-05 17:01:56 -0400

    Adição de slider

---
+++ JS/logicas.js
@@ -0,0 +1,17 @@
+const resButton = document.getElementById('plano-botao');
+const empButton = document.getElementById('plano-botaoemp');
+const playButton = document.getElementById('play-store');
+const appleButton = document.getElementById('app-store');
+
+resButton.addEventListener('click', function () {
+    window.open('https://wa.me/559284214072', '_blank');
+});
+empButton.addEventListener('click', function () {
+    window.open('https://wa.me/559284214072', '_blank');
+});
+playButton.addEventListener('click', function () {
+    window.open('https://play.google.com/store/apps/details?id=br.com.topsapp.topcliente.tcmrjomar', '_blank');
+});
+appleButton.addEventListener('click', function () {
+    window.open('https://apps.apple.com/br/app/mr-jomar/id6448962045', '_blank');
+});
---
+++ JS/slider.js
@@ -0,0 +1,40 @@
+
+const slider = document.querySelectorAll('.item');
+const btnPrev = document.getElementById('Prev');
+const btnNext = document.getElementById('Next');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('current-item'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('current-item')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+setInterval(() => {
+  nextSlider(currentSlide + 1);
+}, 4000); // Altere 4000 para o tempo desejado (em ms)
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
