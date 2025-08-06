730e74d0dc3c0f9a36771c37cf3df9d20375db4b
FDarko74 <frederico.marcel@gmail.com>
2025-08-06 17:32:26 -0400

    Remoção de carrosel e adição de slider infinito

--- JS/slider.js
+++ JS/slider.js
@@ -1,39 +1,49 @@
-const slider = document.querySelectorAll('.item');
-const btnPrev = document.getElementById('Prev');
-const btnNext = document.getElementById('Next');
+// const slider = document.querySelectorAll('.item');
+// const btnPrev = document.getElementById('Prev');
+// const btnNext = document.getElementById('Next');
 
-let currentSlide = 0;
+// let currentSlide = 0;
 
-function hideSlider() {
-  slider.forEach(item => item.classList.remove('current-item'))
-}
+// function hideSlider() {
+//   slider.forEach(item => item.classList.remove('current-item'))
+// }
 
-function showSlider() {
-  slider[currentSlide].classList.add('current-item')
-}
+// function showSlider() {
+//   slider[currentSlide].classList.add('current-item')
+// }
 
-function nextSlider() {
-  hideSlider()
-  if(currentSlide === slider.length -1) {
-    currentSlide = 0
-  } else {
-    currentSlide++
-  }
-  showSlider()
-}
+// function nextSlider() {
+//   hideSlider()
+//   if(currentSlide === slider.length -1) {
+//     currentSlide = 0
+//   } else {
+//     currentSlide++
+//   }
+//   showSlider()
+// }
 
-function prevSlider() {
-  hideSlider()
-  if(currentSlide === 0) {
-    currentSlide = slider.length -1
-  } else {
-    currentSlide--
-  }
-  showSlider()
-}
-setInterval(() => {
-  nextSlider(currentSlide + 1);
-}, 8000); // Altere 4000 para o tempo desejado (em ms)
+// function prevSlider() {
+//   hideSlider()
+//   if(currentSlide === 0) {
+//     currentSlide = slider.length -1
+//   } else {
+//     currentSlide--
+//   }
+//   showSlider()
+// }
+// setInterval(() => {
+//   nextSlider(currentSlide + 1);
+// }, 8000); // Altere 4000 para o tempo desejado (em ms)
 
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
+// btnNext.addEventListener('click', nextSlider)
+// btnPrev.addEventListener('click', prevSlider)
+
+let elemSCroll = document.querySelector('.slider-wrapper');
+let elemContainer = document.querySelector('.slider');
+let elemFilhos = Array.from(elemContainer.children);
+
+elemFilhos.forEach((item)=> {
+  let elemDoc = item.cloneNode(true);
+
+  elemContainer.appendChild(elemDoc);
+})
